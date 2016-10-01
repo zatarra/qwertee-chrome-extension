33835b590208b2e3563030701504fddf4b85690b
David Gouveia <david.gouveia@blip.pt>
2016-10-01 16:20:59 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # qwertee-chrome-extension
 Chrome extension to reveal the price of all the tshirts without having to hover images first. 
+
+## How does it work ?
+This is a pretty basic chrome extension. If you open qwertee.com and inspect DOM, you'll see that the prices are there, but all of them are hidden and waiting you to hover the images which will make it seitch from display:none to display:block.
+
+What this extension does is to set the display mode of the price elements to "block" instead of none and do it with a higher priority so that it doesn't get removed anymore.
---
+++ content.js
@@ -0,0 +1,19 @@
+/*
+  "Developed" by David Gouveia <david.gouveia [_at_] gmail.com>
+  https://www.davidgouveia.net
+*/
+
+//After inspecting DOM it's pretty clear that the prices are simply hidden. Switching frim display: none to display:block is enough to see all the prices.
+
+var css = '.hover-info { display: block !important; } .product-price { display: block !important; }',
+    head = document.head || document.getElementsByTagName('head')[0],
+    style = document.createElement('style');
+
+style.type = 'text/css';
+if (style.styleSheet){
+  style.styleSheet.cssText = css;
+} else {
+  style.appendChild(document.createTextNode(css));
+}
+
+head.appendChild(style);
